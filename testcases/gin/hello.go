9b9e7689bc9713ca1a094067c4b9f88dfb1a7684
cainmusic <freeboynil@gmail.com>
2023-11-16 18:25:04 +0800

    add test case for gin

--- testcases/gin/hello.go
+++ testcases/gin/hello.go
@@ -19,5 +19,13 @@ func main() {
 		c.String(200, c.Param("str"))
 	})
 
+	r.GET("/call_render_several_times", func(c *gin.Context) {
+		c.String(200, "hello")
+		c.String(200, "abc")
+		c.JSON(200, gin.H{"a": 1, "b": 2})
+		c.String(100, "def")
+		c.String(500, "ghi")
+	})
+
 	r.Run()
 }
