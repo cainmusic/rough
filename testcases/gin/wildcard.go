719a59b97c977a7e6fbef4836f2fb0ce0519b898
cainmusic <freeboynil@gmail.com>
2023-12-12 15:53:02 +0800

    modify gin testcase

--- testcases/gin/wildcard.go
+++ testcases/gin/wildcard.go
@@ -10,12 +10,18 @@ import (
 func main() {
 	r := gin.Default()
 
+	//r.GET("/:function", quickResponseUrlString)
+
 	// param不排斥兄弟节点
-	r.GET("/user/biliabc", quickResponseUrlString)
+	//r.GET("/user/biliabc", quickResponseUrlString)
+	r.GET("/user/biliabc/", quickResponseUrlString)
 
 	r.GET("/user/bili:id", func(c *gin.Context) {
 		log.Println(c.Param("id"))
 	}, quickResponseUrlString)
+
+	//r.GET("/user/bili:ids/", quickResponseUrlString)
+	r.GET("/user/bili:id/", quickResponseUrlString)
 
 	// 跟:id冲突
 	//r.GET("/user/bili:name", quickResponseUrlString)
@@ -35,6 +41,8 @@ func main() {
 	//r.GET("/static/aaa:file", quickResponseUrlString)
 	//r.GET("/static/hello", quickResponseUrlString)
 
+	// 会导致index out of range的报错
+	//r.GET("/static/", quickResponseUrlString)
 	r.GET("/static/*file", func(c *gin.Context) {
 		log.Println(c.Param("file"))
 	}, quickResponseUrlString)
@@ -43,7 +51,29 @@ func main() {
 	//r.GET("/html/*html/hello", quickResponseUrlString)
 	//r.GET("/html/*html/", quickResponseUrlString)
 
+	go server2()
+
+	go server3()
+
 	r.Run()
+}
+
+func server2() {
+	r := gin.Default()
+
+	r.GET("/*function", quickResponseUrlString)
+
+	r.Run(":8081")
+}
+
+func server3() {
+	r := gin.Default()
+	// panic: runtime error: index out of range [0] with length 0
+	//r.GET("/static/", func(c *gin.Context) { c.String(200, "static") })
+	r.GET("/static", func(c *gin.Context) { c.String(200, "static") })
+	r.GET("/static/*file", func(c *gin.Context) { c.String(200, "static file") })
+
+	r.Run(":8082")
 }
 
 func quickResponseUrlString(c *gin.Context) {
